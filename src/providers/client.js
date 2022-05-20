434cd972c965426560dc9d5478928abf62f71fa0
Tiago Santos <tiago.silva.qa@gmail.com>
2022-05-19 22:11:09 -0300

    formulário de cadastro de pets

--- src/providers/client.js
+++ src/providers/client.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const apiURL = process.env.REACT_APP_API_URL;
-console.log(apiURL);
 
 const instance = axios.create({
   baseURL: apiURL,
--- src/routes/ProtectedPage.js
+++ src/routes/ProtectedPage.js
@@ -13,10 +13,58 @@ export default function ProtectedPage() {
     request();
   });
 
-  return <Title>Cadastre seu Pet</Title>;
+  return (
+    <>
+      <Title>Cadastre seu Pet</Title>
+      <Form onSubmit={""}>
+        <Fields>
+          Nome:
+          <input type="text" name="nome" />
+        </Fields>
+        <Fields>
+          Raça:
+          <input type="text" name="raca" />
+        </Fields>
+        <Fields>
+          Idade:
+          <input type="number" name="idade" />
+        </Fields>
+        <Fields>
+          Especie:
+          <input type="number" name="especie" />
+        </Fields>
+        <Fields>
+          Gênero:
+          <select name="genero" required>
+            <option selected value="none"></option>
+            <option value="Female">Fêmea</option>
+            <option value="Male">Macho</option>
+          </select>
+        </Fields>
+        <Fields>
+          Link da foto do pet:
+          <input type="text" name="raca" />
+        </Fields>
+        <button type="submit">Cadastrar</button>
+      </Form>
+    </>
+  );
 }
 
 const Title = styled.h3`
   margin: 5px;
   font-size: 18px;
+`;
+
+const Fields = styled.label`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  max-width: 800px;
 `;
